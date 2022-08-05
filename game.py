8eb5a19c3b6e156e1434332865c281daa64d9617
PatriciJo <sebastian.patrici@gmail.com>
2022-08-05 05:33:47 +0200

    update

--- game.py
+++ game.py
@@ -1,63 +1,134 @@
 # Settings
 
-from pickle import FALSE, TRUE
 import words
 wordList = words.words.upper().split(' ')
 
 startLifes = 5
 startTimer = 20
 minTimer = 7
-timerDecreaseInPercent = 10
+timerDecreaseInSec = 1
 pointsIncrement = 50
 
 # Imports 
 
 from random import randrange
+import time
 
-# Vars
+# Global Vars
 
 lifes = startLifes
 points = 0
 timer = startTimer
 currentLetters = ''
+randomWord = ''
 
 # Functions
 
 def setLetters():
-
-    #Buchstaben werden aus den wörtern gefiltert um sicher zu stellen das es mindestens eine richtige lösungt gibt.
-
+    global currentLetters 
+    global randomWord
     randomWord = wordList[randrange(len(wordList))]
     lettersStartPoint = randrange(len(randomWord)-3)
 
-    global currentLetters 
+    
     currentLetters = randomWord[lettersStartPoint:lettersStartPoint+3].upper()
 
 
-def checkWord(letters, word):
+def checkWord(letters, word, timeLeft):
+
+    global points
+    global timer
+
     if letters not in word:
-        print('wort enthält die buchstaben nicht')
+        print('')
+        print(word + ' enthaelt die Buchstabenkombination ' + letters + ' nicht')
+    
     else:
         if word in wordList:
-            print('gutes wort')
+            if timeLeft > 0:
+                points = points + pointsIncrement * timeLeft
+                if timer > minTimer:
+                    timer = timer-timerDecreaseInSec
+                print('')
+                print(word + ' ist Richtig! Dir werden ' + str(pointsIncrement * (startTimer - timeLeft)) + ' Punkte gutgeschrieben. Du hast jetzt ' + str(points) + ' Punkte')
+                input('Starte die naechste Runde mit Enter')
+                runRound()
+            
+            
         else:
-            print('wort gibt es nicht')
+            print('')
+            print('Wir konnten kein Wort namens ' + word + ' finden')
+   
         
 
-setLetters()
+def runGame():
+    print('')
+    print('Willkommen zum Python Word Bomb game!')
+    print('')
+    print('Finde ein Wort welches die Buchstabenkombination enthält.')
+    print('')
+    input('Starte das Spiel mit Enter')
+    while lifes > 0:
+        runRound()
+
+def runRound():
+    setLetters()
+    start = time.time()
+    print('')
+    while time.time() - start < timer:
+      
+        timeLeft = timer - int(time.time() - start)
+        
+        print('Du hast noch ' + str(timeLeft) + ' sekunden zeit.')
+        userInput = input('Dein Wort mit '+ currentLetters +': ')
+        timeLeft = timer - int(time.time() - start) #muss gemacht werden um die time nach dem input zu erfassen
+        checkWord(currentLetters, userInput.upper(), timeLeft)
+
+    toLate()
+
+
+def toLate():
+
+    global lifes 
+    global points 
+    global timer 
+    global currentLetters 
+
+    lifes = lifes - 1
+
+    print(randomWord + ' waere eine möglichkeit gewesen.')
+
+    if lifes > 0:
+        print('')
+        
+        print('Du warst zu langsahm und hast noch ' + str(lifes) +' leben.')
+        print('')
+        input('Beginne die nächste Runde mit Enter')
+        runRound()
+    else:
+        print('')
+        print('')
+        print('GAMEOVER du hast ' + str(points) +' Punkte erreicht.')
+        input('Starte das Spiel mit Enter')
+        lifes = startLifes
+        points = 0
+        timer = startTimer
+        currentLetters = ''
+        runGame()
+
+runGame()
 
 
 
-print('Willkommen zum Python Word Bomb game!')
-print('')
-print('#####################################')
-print('')
-print('Tippe ein wort ein welches folgende Buchstabenenthaelt')
-print('')
-print(currentLetters)
-print('')
-userInput = input()
-checkWord(currentLetters, userInput.upper())
+
+
+
+
+
+
+
+
+
 
 
 
