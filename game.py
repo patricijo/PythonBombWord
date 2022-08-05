8709547d01016187bc5f7616e7597e95d3c0f6ca
PatriciJo <sebastian.patrici@gmail.com>
2022-08-05 02:26:35 +0200

    update

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 # Settings
 
+from pickle import FALSE, TRUE
 import words
-wordList = words.words.split(' ')
+wordList = words.words.upper().split(' ')
 
 startLifes = 5
 startTimer = 20
@@ -22,20 +23,45 @@ currentLetters = ''
 
 # Functions
 
+def setLetters():
 
-def getLetters():
     #Buchstaben werden aus den wörtern gefiltert um sicher zu stellen das es mindestens eine richtige lösungt gibt.
+
     randomWord = wordList[randrange(len(wordList))]
+    lettersStartPoint = randrange(len(randomWord)-3)
 
-    
-    print(randomWord)
+    global currentLetters 
+    currentLetters = randomWord[lettersStartPoint:lettersStartPoint+3].upper()
 
 
-getLetters()
+def checkWord(letters, word):
+    if letters not in word:
+        print('wort enthält die buchstaben nicht')
+    else:
+        if word in wordList:
+            print('gutes wort')
+        else:
+            print('wort gibt es nicht')
+        
+
+setLetters()
 
 
 
 print('Willkommen zum Python Word Bomb game!')
+print('')
+print('#####################################')
+print('')
+print('Tippe ein wort ein welches folgende Buchstabenenthaelt')
+print('')
+print(currentLetters)
+print('')
+userInput = input()
+checkWord(currentLetters, userInput.upper())
 
-print(wordList[4])
+
+
+
+
+
 
